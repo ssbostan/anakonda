74886d3deba3f1f9494f17a0a30dabc559f18a69
Saeid Bostandoust <ssbostan@yahoo.com>
2023-04-20 20:05:42 +0100

    add marshmallow instance

--- anakonda/anakonda.py
+++ anakonda/anakonda.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, Flask
+from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
@@ -7,6 +8,7 @@ from .config import Config
 
 db = SQLAlchemy()
 mg = Migrate()
+ma = Marshmallow()
 
 apiv1_bp = Blueprint("apiv1_bp", __name__, url_prefix="/api/v1")
 apiv1 = Api(apiv1_bp)
@@ -19,5 +21,6 @@ def create_app():
     app.config.from_object(Config)
     db.init_app(app)
     mg.init_app(app, db)
+    ma.init_app(app)
     app.register_blueprint(apiv1_bp)
     return app
